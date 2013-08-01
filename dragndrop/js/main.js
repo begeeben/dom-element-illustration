84c151cd9570867e1f60fa0dfd6acb720ed8b90a
Yi-Fan Liao <begeeben@gmail.com>
2013-08-01 23:00:55 +0800

    Fixed for mobile.

--- dragndrop/js/main.js
+++ dragndrop/js/main.js
@@ -1,3 +1,10 @@
   $(function() {
     $( ".draggable" ).draggable();
   });
+
+$(document).ready(function() {
+        $(".draggable").draggable();
+        $('body>div').bind("dragstart", function(event, ui){
+        event.stopPropagation();
+        });
+    });
