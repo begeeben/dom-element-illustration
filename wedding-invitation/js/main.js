526210218cb28407dc3044914bda1201846d9912
Yi-Fan Liao <begeeben@gmail.com>
2013-09-18 17:27:53 +0800

    Add buttons for style changing.

--- wedding-invitation/js/main.js
+++ wedding-invitation/js/main.js
@@ -1,3 +1,59 @@
+(function() {
+    var backgroundStyleList = [
+        'frenchstucco',
+        'rockywall',
+        'wall4',
+        'whitey',
+        'xv'
+    ];
+
+    var currentBackgroundStyleIndex = 0;
+
+    var boardStyleList = [
+        'wood',
+        'wood-stripe'
+    ];
+
+    var currentPhotoBoardStyleIndex = 0;
+    var currentInvitationBoardStyleIndex = 1;
+
+    $('#change-background').click(function() {
+        $('body').removeClass(backgroundStyleList[currentBackgroundStyleIndex]);
+        currentBackgroundStyleIndex += 1;
+        currentBackgroundStyleIndex = currentBackgroundStyleIndex > backgroundStyleList.length - 1 ? 0 : currentBackgroundStyleIndex;
+        $('body').addClass(backgroundStyleList[currentBackgroundStyleIndex]);
+    });
+
+    $('#change-photo-board').click(function() {
+        $('.photo-board').removeClass(boardStyleList[currentPhotoBoardStyleIndex]);
+        currentPhotoBoardStyleIndex += 1;
+        currentPhotoBoardStyleIndex = currentPhotoBoardStyleIndex > boardStyleList.length - 1 ? 0 : currentPhotoBoardStyleIndex;
+        $('.photo-board').addClass(boardStyleList[currentPhotoBoardStyleIndex]);
+    });
+
+    $('#change-invitation-board').click(function() {
+        $('#invitation-board').removeClass(boardStyleList[currentInvitationBoardStyleIndex]);
+        currentInvitationBoardStyleIndex += 1;
+        currentInvitationBoardStyleIndex = currentInvitationBoardStyleIndex > boardStyleList.length - 1 ? 0 : currentInvitationBoardStyleIndex;
+        $('#invitation-board').addClass(boardStyleList[currentInvitationBoardStyleIndex]);
+    });
+
+    $('#heart-beat').click(function() {
+        $('.loading').fadeIn(2000).fadeOut(2000);
+    });
+
+    document.onclick = function (evt) {
+        var tgt = (evt && evt.target) || event.srcElement,
+            scr = document.body.scrollTop;
+
+        if (tgt.tagName == "A" && tgt.href.slice(-1) == "#") {
+            window.location.href = "#";
+            document.body.scrollTop = scr;           
+            return false;
+        }
+    };
+})();
+
 $(window).load(function() {
     skrollr.init({
         // edgeStrategy: 'set',
