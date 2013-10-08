46409701efafd204b80f3a3f5be53c7426167ad5
Yi-Fan Liao <begeeben@gmail.com>
2013-10-08 17:57:10 +0800

    Fix carousel flicker.

--- carousel/app/scripts/main.js
+++ carousel/app/scripts/main.js
@@ -5,35 +5,36 @@ $(".left").one('click', moveRight);
 function moveLeft(e){
     // $(this).removeClass("right").addClass("center");
     $(".carousel .center").addClass("left-active").one('transitionend', function(e){
-        $(this).removeClass('center left-active').addClass('left');
-        $(".left").one('click', moveRight);
+        $(this).addClass('left').removeClass('center left-active');
+        $(this).one('click', moveRight);
     });
     $(".carousel .right").addClass("center-active").one('transitionend', function(e){
-        $(this).removeClass('right center-active').addClass('center');
+        $(this).addClass('center').removeClass('right center-active');
     });
     $(".carousel .left").addClass("left-most-active").one('transitionend', function(e){
-        $(this).removeClass('left left-most-active').addClass('left-most');
+        $(this).addClass('left-most');
+        $(this).removeClass('left left-most-active');
     });
     $(this).next().addClass('right-active').one('transitionend', function(e){
-        $(this).removeClass('right-active right-most').addClass('right');
-        $(".right").one('click', moveLeft);
+        $(this).addClass('right').removeClass('right-active right-most');
+        $(this).one('click', moveLeft);
     });
 }
 
 function moveRight(e){
     // $(this).removeClass("right").addClass("center");
     $(".carousel .center").addClass("right-active").one('transitionend', function(e){
-        $(this).removeClass('center right-active').addClass('right');
-        $(".right").one('click', moveLeft);
+        $(this).addClass('right').removeClass('center right-active');
+        $(this).one('click', moveLeft);
     });
     $(".carousel .right").addClass("right-most-active").one('transitionend', function(e){
-        $(this).removeClass('right right-most-active').addClass('right-most');
+        $(this).addClass('right-most').removeClass('right right-most-active');
     });
     $(".carousel .left").addClass("center-active").one('transitionend', function(e){
-        $(this).removeClass('left center-active').addClass('center');
+        $(this).addClass('center').removeClass('left center-active');
     });
     $(this).prev().addClass('left-active').one('transitionend', function(e){
-        $(this).removeClass('left-active left-most').addClass('left');
-        $(".left").one('click', moveRight);
+        $(this).addClass('left').removeClass('left-active left-most');
+        $(this).one('click', moveRight);
     });
 }
