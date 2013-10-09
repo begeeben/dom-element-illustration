09848b507e4d3a5f04524f13fae3670d837779de
Yi-Fan Liao <begeeben@gmail.com>
2013-10-09 17:10:57 +0800

    Revise user click handling.

--- carousel/app/scripts/main.js
+++ carousel/app/scripts/main.js
@@ -1,10 +1,35 @@
 var transitions = 0;
+var nextClicks = 0;
 
-$('.carousel').on('click', '.right', moveLeft);
+$('.carousel').on('click', '.right', function(){
+    if (nextClicks > 0) {
+        nextClicks = 0;
+    }
 
-$('.carousel').on('click', '.left', moveRight);
+    if (transitions < 1) {
+        moveLeft();
+    } else if ($(this).next().length > 0) {
+        nextClicks -= 1;
+    } else {
+        nextClicks = 0;
+    }
+});
 
-$('.carousel').on('transitionend', function(e){
+$('.carousel').on('click', '.left', function() {
+    if (nextClicks < 0) {
+        nextClicks = 0;
+    }
+
+    if (transitions < 1) {
+        moveRight();
+    } else if ($(this).prev().length > 0) {
+        nextClicks += 1;
+    } else {
+        nextClicks = 0;
+    }
+});
+
+$('.carousel').on('transitionend', function(){
     // console.debug(e);
     // console.debug(transitions);
 
@@ -16,25 +41,33 @@ $('.carousel').on('transitionend', function(e){
         $(this).find('.left-most-active').addClass('left-most').removeClass('left left-most-active');
         $(this).find('.right-active').addClass('right').removeClass('right-active center right-most');
         $(this).find('.right-most-active').addClass('right-most').removeClass('right right-most-active');
+
+        if (nextClicks > 0) {
+            moveRight();
+        } else if (nextClicks < 0) {
+            moveLeft();
+        }
     }
 });
 
 function addTransition() { transitions += 3; }
 
-function moveLeft(e){
-    if (transitions < 1) {
+function moveLeft(){
+    if ($('.right').length > 0) {
+        // console.debug('move left');
         $(".carousel .center").addClass("left-active").each(addTransition);
         $(".carousel .right").addClass("center-active").each(addTransition);
         $(".carousel .left").addClass("left-most-active").each(addTransition);
-        $(this).next().addClass('right-active').each(addTransition);
+        $('.right').next().addClass('right-active').each(addTransition);
     }
 }
 
-function moveRight(e){
-    if (transitions < 1) {
+function moveRight(){
+    if ($('.carousel .left').length > 0) {
+        // console.debug('move right');
         $(".carousel .center").addClass("right-active").each(addTransition);
         $(".carousel .right").addClass("right-most-active").each(addTransition);
         $(".carousel .left").addClass("center-active").each(addTransition);
-        $(this).prev().addClass('left-active').each(addTransition);
+        $('.left').prev().addClass('left-active').each(addTransition);
     }
 }
