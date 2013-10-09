0631b87a2cb66448a5929d34f3d049a0cc8723f2
Yi-Fan Liao <begeeben@gmail.com>
2013-10-09 11:37:28 +0800

    Working on carousel event and style.

--- carousel/app/scripts/main.js
+++ carousel/app/scripts/main.js
@@ -1,38 +1,59 @@
-$(document).on('click', '.carousel .right', moveLeft);
+$('.carousel').on('click', '.right', moveLeft);
 
-$(document).on('click', '.carousel .left', moveRight);
+$('.carousel').on('click', '.left', moveRight);
 
-$(document).on('transitionend', function(e){
+$('.carousel').on('transitionend', function(e){
     console.debug(e);
+
+    // switch(e.target.className) {
+    //     case '':
+    // }
+    var className = e.target.className;
+    if (className.indexOf('left-most-active')) {
+        className = 'left-most';
+    } else if (className.indexOf('right-most-active')) {
+        className = 'right-most';
+    } else if (className.indexOf('right-active')) {
+        className = 'right';
+    } else if (className.indexOf('left-active')) {
+        className = 'left';
+    } else if (className.indexOf('center-active')) {
+        className = 'center';
+    }
+
+    // e.target.className = className;
 });
 
 function moveLeft(e){
-    $(".carousel .center").addClass("left-active").one('transitionend', function(e){
-        $(this).addClass('left').removeClass('center left-active');
-    });
-    $(".carousel .right").addClass("center-active").one('transitionend', function(e){
-        $(this).addClass('center').removeClass('right center-active');
-    });
-    $(".carousel .left").addClass("left-most-active").one('transitionend', function(e){
-        $(this).addClass('left-most');
-        $(this).removeClass('left left-most-active');
-    });
-    $(this).next().addClass('right-active').one('transitionend', function(e){
-        $(this).addClass('right').removeClass('right-active right-most');
-    });
+    $(".carousel .center").addClass("left-active");
+    // .one('transitionend', function(e){
+    //     // $(this).addClass('left').removeClass('center left-active');
+    // });
+    $(".carousel .right").addClass("center-active");
+    // .one('transitionend', function(e){
+    //     // $(this).addClass('center').removeClass('right center-active');
+    // });
+    $(".carousel .left").addClass("left-most-active");
+    // .one('transitionend', function(e){
+    //     // $(this).addClass('left-most').removeClass('left left-most-active');
+    // });
+    $(this).next().addClass('right-active');
+    // .one('transitionend', function(e){
+    //     // $(this).addClass('right').removeClass('right-active right-most');
+    // });
 }
 
 function moveRight(e){
     $(".carousel .center").addClass("right-active").one('transitionend', function(e){
-        $(this).addClass('right').removeClass('center right-active');
+        // $(this).addClass('right').removeClass('center right-active');
     });
     $(".carousel .right").addClass("right-most-active").one('transitionend', function(e){
-        $(this).addClass('right-most').removeClass('right right-most-active');
+        // $(this).addClass('right-most').removeClass('right right-most-active');
     });
     $(".carousel .left").addClass("center-active").one('transitionend', function(e){
-        $(this).addClass('center').removeClass('left center-active');
+        // $(this).addClass('center').removeClass('left center-active');
     });
     $(this).prev().addClass('left-active').one('transitionend', function(e){
-        $(this).addClass('left').removeClass('left-active left-most');
+        // $(this).addClass('left').removeClass('left-active left-most');
     });
 }
