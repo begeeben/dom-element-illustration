d2141b02abde4e70fae7852628b74edb981d8ba9
Yi-Fan Liao <begeeben@gmail.com>
2014-05-02 17:16:43 +0800

    Clean up comments.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,81 +2,10 @@
 
 var container, contentElement, wrapperElement,
 	clientHeightElement, offsetHeightElement, scrollHeightElement,
-	// defaultScrollHeightElementWidth,
-	// defaultScrollWidthElementHeight,
 	clientWidthElement, offsetWidthElement, scrollWidthElement,
 	clientTopElement, offsetTopElement, scrollTopElement,
 	clientLeftElement, offsetLeftElement, scrollLeftElement,
 	mockContentElement;
-	// animationId;
-
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
-// MIT license
-
-// (function() {
-// 	var lastTime = 0;
-// 	var vendors = ['ms', 'moz', 'webkit', 'o'];
-// 	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-// 		window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-// 		window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-// 	}
-
-// 	if (!window.requestAnimationFrame) {
-// 		window.requestAnimationFrame = function(callback, element) {
-// 			var currTime = new Date().getTime();
-// 			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-// 			var id = window.setTimeout(function() {
-// 					callback(currTime + timeToCall);
-// 				},
-// 				timeToCall);
-// 			lastTime = currTime + timeToCall;
-// 			return id;
-// 		};
-// 	}
-
-// 	if (!window.cancelAnimationFrame) {
-// 		window.cancelAnimationFrame = function(id) {
-// 			clearTimeout(id);
-// 		};
-// 	}
-// }());
-
-// function addClass(el, className) {
-// 	if (el.classList) {
-// 		el.classList.add(className);
-// 	} else {
-// 		el.className += ' ' + className;
-// 	}
-// }
-
-// function removeClass(el, className) {
-// 	if (el.classList) {
-// 		el.classList.remove(className);
-// 	} else {
-// 		el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-// 	}
-// }
-
-function toggleClass(el, className) {
-	if (el.classList) {
-		el.classList.toggle(className);
-	} else {
-		var classes = el.className.split(' ');
-		var existingIndex = classes.indexOf(className);
-
-		if (existingIndex >= 0) {
-			classes.splice(existingIndex, 1);
-		} else {
-			classes.push(className);
-		}
-
-		el.className = classes.join(' ');
-	}
-}
 
 function getY(element, container) {
 	var y = 0;
@@ -163,7 +92,6 @@ function updateMock(wrapperStyle) {
 
 	offsetLeftElement.style.left = wrapperX - marginLeft + 'px';
 	offsetLeftElement.querySelector('.offset-left-marker').style.width = wrapperElement.offsetLeft + 'px';
-	// offsetLeftElement.querySelector('.offset-left-marker').style.height = 30 + marginTop + 'px';
 
 	scrollLeftElement.style.left = scrollWidthElement.style.left;
 	scrollLeftElement.querySelector('.scroll-left-marker').style.width = wrapperElement.scrollLeft + 'px';
@@ -190,27 +118,7 @@ function updateInfo(wrapperStyle) {
 
 	clientTopElement.querySelector('.top-text').innerHTML = 'clientTop: ' + wrapperElement.clientTop + 'px';
 	offsetTopElement.querySelector('.top-text').innerHTML = 'offsetTop: ' + wrapperElement.offsetTop + 'px';
-
-	// updateMock();
 }
-
-function toggleWidth() {
-	toggleClass(clientWidthElement, 'is-hidden');
-	toggleClass(offsetWidthElement, 'is-hidden');
-	toggleClass(scrollWidthElement, 'is-hidden');
-}
-
-function toggleHeight() {
-	toggleClass(clientHeightElement, 'is-hidden');
-	toggleClass(offsetHeightElement, 'is-hidden');
-	toggleClass(scrollHeightElement, 'is-hidden');
-}
-
-// function update() {
-// 	updateInfo();
-// 	// updateMock();
-// 	animationId = requestAnimationFrame(update);
-// }
 
 function updateDimension(prop, value) {
 	wrapperElement.style[prop] = value + 'px';
@@ -218,7 +126,6 @@ function updateDimension(prop, value) {
 	document.querySelector('#' + prop + '-value').innerHTML = value;
 	updateMock(wrapperStyle);
 	updateInfo(wrapperStyle);
-	// syncContentDimension();
 }
 
 function updateContentDimension(prop, value) {
@@ -228,28 +135,6 @@ function updateContentDimension(prop, value) {
 	document.querySelector('#content-' + prop + '-value').innerHTML = value;
 	updateMock(wrapperStyle);
 	updateInfo(wrapperStyle);
-}
-
-// function syncContentDimension() {
-// 	var wrapperStyle = getComputedStyle(wrapperElement);
-// 	var padding = parseInt(wrapperStyle.padding);
-// 	if (wrapperElement.scrollWidth===wrapperElement.clientWidth && wrapperElement.scrollWidth>contentElement.offsetWidth + 2 * padding) {
-// 		contentElement.style.width = wrapperElement.clientWidth - 2 * padding + 'px';
-// 	}
-// }
-
-function toggleWideContent() {
-	// toggleClass(wrapperElement, 'wide');
-	updateMock();
-	// cancelAnimationFrame(animationId);
-	// animationId = requestAnimationFrame(update);
-}
-
-function toggleLongContent() {
-	// toggleClass(wrapperElement, 'long');
-	updateMock();
-	// cancelAnimationFrame(animationId);
-	// animationId = requestAnimationFrame(update);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -270,22 +155,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	offsetLeftElement = document.querySelector('.offset-left');
 	scrollLeftElement = document.querySelector('.scroll-left');
 
-	// defaultScrollHeightElementWidth = parseInt(getComputedStyle(scrollHeightElement).width);
-	// defaultScrollWidthElementHeight = parseInt(getComputedStyle(scrollWidthElement).height);
-
 	var wrapperStyle = getComputedStyle(wrapperElement);
 	updateMock(wrapperStyle);
 	updateInfo(wrapperStyle);
-
-	// wrapperElement.addEventListener('transitionend', function() {
-	// 	console.debug('transition end');
-	// 	cancelAnimationFrame(animationId);
-	// });
-
-	// animationId = requestAnimationFrame(update);
-
-	// wrapperElement.onscroll = updateMock;
-
 });
 
 function resetDimensions() {
@@ -307,7 +179,6 @@ function resetDimensions() {
 }
 
 $('#view-properties input:checkbox').on('change', function() {
-	// toggleClass(this, 'is-hidden');
 	$('.element-layer').find('.' + this.value).toggleClass('is-hidden');
 });
 
